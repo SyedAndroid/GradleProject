31346f32a7746aa7504043dc7d5f5bd43e90401b
googlite123 <syed.usman@yahoo.com>
2017-08-24 03:12:06 +0200

    first commit

---
+++ androidlibrary/src/main/java/com/example/androidlibrary/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidlibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String joke = bundle.getString("joke","No joke available");
+
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+
+        TextView jokeView =(TextView) findViewById(R.id.joke);
+        jokeView.setText(joke);
+
+    }
+}
